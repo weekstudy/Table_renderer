2a6b0ecafafbee9002a1b1254b2eb501c3de5e6c
weekstudy <1373843746@qq.com>
2021-10-21 18:03:58 +0800

    'modify log file'

--- log/log.py
+++ log/log.py
@@ -9,20 +9,30 @@
  
 """
 
-# import logging
+import logging
+import sys
 import logging.handlers
 import traceback
+import datetime
 
 
 class Logger(object):
     def __init__(self, name='root', fp=None):
         self.logger = logging.getLogger(name)
-        self.logger.setLevel(logging.DEBUG)
+        self.logger.setLevel(logging.INFO)
+        formatter = logging.Formatter(
+            '[%(asctime)s] %(name)s %(levelname)s: %(message)s',
+            datefmt="%Y/%m/%d %H:%M:%S")
+        # 让容器时间与宿主机时间同步
+        logging.Formatter.converter = std_time
+        # 输出到屏幕
+        stream_handler = logging.StreamHandler(stream=sys.stdout)
+        stream_handler.setLevel(logging.INFO)
+        stream_handler.setFormatter(formatter)
+        self.logger.addHandler(stream_handler)
+        # 输出到文件
         f_handler = logging.FileHandler(fp, mode='a')
         f_handler.setLevel(logging.INFO)
-        formatter = logging.Formatter(
-                '[%(asctime)s] %(name)s %(levelname)s: %(message)s',
-                datefmt="%Y/%m/%d %H:%M:%S")
         f_handler.setFormatter(formatter)
         self.logger.addHandler(f_handler)
 
@@ -39,9 +49,15 @@ class Logger(object):
         #     logger.info('ss')
 
 
+def std_time(sec, what):
+    local_time = datetime.datetime.now() + datetime.timedelta(hours=8)
+    return local_time.timetuple()
+
+
 if __name__ == '__main__':
 
     log = Logger(fp='./test.log')
+    log.info('tetst')
 
 
 
